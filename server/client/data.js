77be24bbcae45cbd6b567aa4ff9dd26103f47948
spolina <suryapolina@gmail.com>
2024-01-08 17:04:47 -0600

    Admin Login Token Flow + html templates w/ CSS

---
+++ server/client/data.js
@@ -0,0 +1,54 @@
+const cards = [
+    {
+        id: 1,
+        date: new Date('2020-10-23'),
+        title: "I found Security Vulnerability",
+        description: "Using a locality sensitive hash, you can mark duplicates in millions of items in no time.",
+        hashtags: ["#security", "#hashing", "#vulnerability"]
+    },
+    {
+        id: 2,
+        date: new Date('2021-02-12'),
+        title: "Yoga Day 1",
+        description: "Savasana is the most important yoga pose.",
+        hashtags: ["#yoga", "#exercise", "#well-being"]
+    },
+    {
+        id: 3,
+        date: new Date('2021-05-16'),
+        title: "My thoughts on various programming languages",
+        description: "Some ill-informed remarks on various programming languages.",
+        hashtags: ["#programming", "#languages", "#coding"]
+    },
+    {
+        id: 4,
+        date: new Date('2023-07-22'),
+        title: "Chocolate Cake Recipe",
+        description: "A recipe for a delicious chocolate cake.",
+        hashtags: ["#chocolate", "#cake", "#dessert"]
+    },
+    {
+        id: 5,
+        date: new Date('2023-12-15'),
+        title: "Tampa Bay",
+        description: "Nice beaches. Laid back",
+        hashtags: ["#vacation", "#tampa", "#friends"]
+    },
+    {
+        id: 6,
+        date: new Date('2024-01-01'),
+        title: "Rajasthani Wedding",
+        description: "Wedding in the desert",
+        hashtags: ["#india", "#vacation", "#adventure"]
+    },
+    {
+        id: 7,
+        date: new Date('2021-04-23'),
+        title: "CS471",
+        description: "Operating Systems",
+        hashtags: ["#cs", "#os", "#school"]
+    },
+    // ... more card objects
+  ];
+
+export default cards;
